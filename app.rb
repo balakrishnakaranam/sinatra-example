589f7846dfc1e946e8ffa0a30009147cbc6a7007
root <root@ip-172-31-26-125.ap-southeast-1.compute.internal>
2017-07-10 01:05:00 -0400

    updated

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/' do
   "the time where this server lives is #{Time.now}
-    <br /><br />testing s2i - 20170710 <a href=\"/agent\">user_agent</a>"
+    <br /><br />testing s2i - 201707101304 <a href=\"/agent\">user_agent</a>"
 end
 
 get '/agent' do
